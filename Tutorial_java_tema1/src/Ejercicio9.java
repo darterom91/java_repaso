06d66160b346f52a8c451f0265d44709dcfbb761
David Artero <d.arterom@gmail.com>
2017-06-04 22:49:18 +0200

    Ejercicio 9

---
+++ Tutorial_java_tema1/src/Ejercicio9.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+public class Ejercicio9 {
+	Scanner s = new Scanner (System.in);
+	public void menu(){
+		int option=0;
+		do{
+			System.out.println("Menu Ejercicio9\n\n1 Busca Palabra\n2 Lola Remplazar\n3 Hello Remplazar\n4 Bye Remplazar\n5 salir");
+			option=s.nextInt();
+			switch(option){
+			case 1:
+				System.out.println("1 Busca Palabra");
+				buscaPalabra();
+				break;
+			case 2:
+				System.out.println("2 Lola Remplazar");
+				lolaRemplazar();
+				break;
+			case 3:
+				System.out.println("3 Hello Remplazar");
+				helloRemplazar();
+				break;
+			case 4:
+				System.out.println("4 Bye Remplazar");
+				byeRempplazar();
+				break;
+			case 5:
+				System.out.println("5 salir");
+				break;
+			default:
+				System.out.println("Error +20001:Option not correct!!");
+				break;
+			}
+
+		}while(option!=5);
+	}
+	
+	public void buscaPalabra(){
+		String frase, frase2[]={};
+		int count=0;
+		System.out.println("introduce una frase: ");
+		s.nextLine();
+		frase=s.nextLine();
+		frase2=frase.split(" ");
+		
+		for(int i=0;i<frase2.length;i++){
+			if(frase2[i].equals("hola")){
+				System.out.println(frase2[i]+" i: "+i);
+				count++;
+			}
+		}
+		System.out.println("cuantos hola hay? "+count);
+		
+		if(count==1){
+			System.out.println("he encontrado hola");
+		}else if(count>1){
+			System.out.println("hay muchos hola...");
+		}else{
+			System.out.println("no se encontro hola");
+		}
+		
+	}
+	
+	public void lolaRemplazar() {
+		String frase, frase2[]={};
+		int count=0;
+		System.out.println("introduce una frase: ");
+		s.nextLine();
+		frase=s.nextLine();
+		frase2=frase.split(" ");
+		
+		for(int i=0;i<frase2.length;i++){
+			if(frase2[i].equals("hola")){
+				frase2[i]="lola";
+			}
+		}
+		
+		for(int i=0;i<frase2.length;i++){
+			System.out.println(frase2[i]);
+		}
+	}
+	
+	public void helloRemplazar() {
+		String frase, frase2[]={};
+		int count=0;
+		System.out.println("introduce una frase: ");
+		s.nextLine();
+		frase=s.nextLine();
+		frase2=frase.split(" ");
+		
+		for(int i=0;i<frase2.length;i++){
+			if(frase2[i].equals("hola")){
+				frase2[i]="hello";
+			}
+		}
+		
+		for(int i=0;i<frase2.length;i++){
+			System.out.println(frase2[i]);
+		}
+		
+	}
+	
+	public void byeRempplazar() {		
+		String frase, frase2[]={};
+		int count=0;
+		System.out.println("introduce una frase: ");
+		s.nextLine();
+		frase=s.nextLine();
+		frase2=frase.split(" ");
+		
+		for(int i=0;i<frase2.length;i++){
+			if(frase2[i].equals("hola")){
+				frase2[i]="bye";
+			}
+		}
+		
+		for(int i=0;i<frase2.length;i++){
+			System.out.println(frase2[i]);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ejercicio9 e9=new Ejercicio9();
+		e9.menu();
+	}
+
+}
